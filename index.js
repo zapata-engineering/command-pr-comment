3319b0c461a13b4173b7c54f9f9c350994e87866
James Clark <james.clark@zapatacomputing.com>
2022-08-05 22:38:01 -0500

    Initial comment action

--- index.js
+++ index.js
@@ -1,20 +1,80 @@
+const github = require('@actions/github');
 const core = require('@actions/core');
-const wait = require('./wait');
+const { spawnSync } = require('node:child_process');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
+  const cmd = core.getInput('command');
+  const messageTemplate = core.getInput('template');
+  const updateText = core.getInput('update-text');
+  const githubToken = core.getInput('github-token');
+
+  const octokit = github.getOctokit(githubToken);
+  const context = github.context;
+
+  if (!context.payload.pull_request) {
+    core.error("This action only runs on Pull Requests");
+    return;
+  }
+
+  var comment = null;
+  var message = "";
+
+  // If we have text to check, we will search the PR for a comment with this text
+  if (updateText){
+    try {
+      for await (const response of octokit.paginate.iterator(
+        octokit.rest.issues.listComments,
+        {
+          issue_number: context.issue.number,
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+        }
+      )) {
+        comment = response.data.find((comment) => comment.body.includes(updateText));
+        if (comment) break;
+      }
+    } catch (_) {
+      // Unable to find the comment, or an error occurred
+      // Continue and make a new comment
+    }
+  }
+
+  // Next, we execute the user's command
+  const splitCmd = cmd.split(" ");
+  const proc = spawnSync(splitCmd[0], splitCmd.slice(1));
+  const cmdOut = proc.stdout.toString();
+
+  // If there's a template, we replace %command% with the output of the command
+  // Otherwise, we return a code block with the command output
+  if(messageTemplate){
+    message = messageTemplate.replace("%command%", cmdOut);
+  } else {
+    message = `\n\`\`\`${cmdOut}\n\`\`\``;
+  }
+  console.log(message);
+
+  // If we found a comment, we update it
+  // Otherwise we create a new comment
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    if (comment) {
+      await octokit.rest.issues.updateComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        comment_id: comment.id,
+        body: message,
+      });
+    } else {
+      await octokit.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: message
+      });
+    }
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(`Error with making comment - ${error.name}: ${error.message}`);
   }
 }
 
