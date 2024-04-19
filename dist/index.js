fc671f758ee11dd8318a8c25544e19eeb8c01b0f
James Clark <70290797+jamesclark-Zapata@users.noreply.github.com>
2024-04-19 18:34:16 +0100

    feat: add stdout and stderr (#62)

--- dist/index.js
+++ dist/index.js
@@ -31325,13 +31325,12 @@ const core = __nccwpck_require__(2186);
 const parse = __nccwpck_require__(277);
 const { spawnSync } = __nccwpck_require__(7718);
 
-
 // most @actions toolkit packages have async methods
 async function run() {
-  const cmd = core.getInput('command');
-  const messageTemplate = core.getInput('template');
-  const updateText = core.getInput('update-text');
-  const githubToken = core.getInput('github-token');
+  const cmd = core.getInput("command");
+  const messageTemplate = core.getInput("template");
+  const updateText = core.getInput("update-text");
+  const githubToken = core.getInput("github-token");
 
   const octokit = github.getOctokit(githubToken);
   const context = github.context;
@@ -31345,7 +31344,7 @@ async function run() {
   var message = "";
 
   // If we have text to check, we will search the PR for a comment with this text
-  if (updateText){
+  if (updateText) {
     try {
       for await (const response of octokit.paginate.iterator(
         octokit.rest.issues.listComments,
@@ -31353,14 +31352,17 @@ async function run() {
           issue_number: context.issue.number,
           owner: context.repo.owner,
           repo: context.repo.repo,
-        }
+        },
       )) {
-        comment = response.data.find((comment) => comment.body.includes(updateText));
+        comment = response.data.find((comment) =>
+          comment.body.includes(updateText),
+        );
         if (comment) break;
       }
     } catch (_) {
       // Unable to find the comment, or an error occurred
       // Continue and make a new comment
+      core.info("No comment found, will create a new one");
     }
   }
 
@@ -31368,13 +31370,20 @@ async function run() {
   const splitCmd = parse(cmd);
   const proc = spawnSync(splitCmd[0], splitCmd.slice(1));
   const cmdOut = proc.stdout.toString();
+  const cmdErr = proc.stderr.toString();
+
+  if (proc.status === null || proc.status !== 0) {
+    core.error(`Command failed: ${cmd}\nstdout:${cmdOut}\nstderr:${cmdErr}`);
+  }
 
   // If there's a template, we replace %command% with the output of the command
   // Otherwise, we return a code block with the command output
-  if(messageTemplate){
+  if (messageTemplate) {
     message = messageTemplate.replace("%command%", cmdOut);
+    message = message.replace("%stdout%", cmdOut);
+    message = message.replace("%stderr%", cmdErr);
   } else {
-    message = `\n\`\`\`${cmdOut}\n\`\`\``;
+    message = `\n\`\`\`${cmdOut}\n\`\`\`<details>\n<summary>stderr</summary>\n\`\`\`${cmdErr}\`\`\`\n</details>`;
   }
   console.log(message);
 
@@ -31393,11 +31402,13 @@ async function run() {
         issue_number: context.issue.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
-        body: message
+        body: message,
       });
     }
   } catch (error) {
-    core.setFailed(`Error with making comment - ${error.name}: ${error.message}`);
+    core.setFailed(
+      `Error with making comment - ${error.name}: ${error.message}`,
+    );
   }
 }
 
